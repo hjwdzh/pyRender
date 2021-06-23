fe47e9d4d5af2fe1103855063ab4d45e4a15238f
Charles Shen <ssa19990208@foxmail.com>
2021-06-23 20:42:03 +0800

    fix_render_bug

--- lib/render.cu
+++ lib/render.cu
@@ -17,6 +17,9 @@ double calculateSignedArea2(const glm::dvec3& a, const glm::dvec3& b, const glm:
 	return ((c.x - a.x) * (b.y - a.y) - (b.x - a.x) * (c.y - a.y));
 }
 
+
+
+
 __host__ __device__ static
 glm::dvec3 calculateBarycentricCoordinate(const glm::dvec3& a, const glm::dvec3& b, const glm::dvec3& c, const glm::dvec3& p) {
 	double beta_tri = calculateSignedArea2(a, p, c);
@@ -42,12 +45,17 @@ double getZAtCoordinate(const glm::dvec3 barycentricCoord, const glm::dvec3& a, 
 		+ barycentricCoord.z * c.z);
 }
 
+
+
 __device__ int CompactRGBToInt(const glm::vec3& rgb) {
 	return ((int)(rgb.x * 255) << 16)
 	+ ((int)(rgb.y * 255) << 8)
 	+ ((int)(rgb.z * 255))
 	+ (255 << 24);
 }
+
+
+
 
 __device__ void atomicExchRGBZ(int* zbuffer, int* image, int z, int rgb) {
 	while (true) {
@@ -83,8 +91,15 @@ __global__ void Render_gpu(glm::vec3* positions, glm::ivec3* indices, int* color
 	glm::dvec3 p2 = glm::dvec3(rotation * positions[face[1]] + translation);
 	glm::dvec3 p3 = glm::dvec3(rotation * positions[face[2]] + translation);
 
-	if (p1.z < 0.02 || p2.z < 0.02 || p3.z < 0.02)
+	glm::dvec3 p1_original = p1;
+	glm::dvec3 p2_original = p2;
+	glm::dvec3 p3_original = p3;
+
+	//如果三个端点深度都是负数，那么一定不可见--剪枝。之前或逻辑并不对
+	if (p1.z < 0.0001 && p2.z < 0.0001 && p3.z < 0.0001)
+	{
 		return;
+	}
 
 	p1.z = 1.0f / p1.z;
 	p2.z = 1.0f / p2.z;
@@ -97,6 +112,8 @@ __global__ void Render_gpu(glm::vec3* positions, glm::ivec3* indices, int* color
 	p3.x = p3.x * p3.z;
 	p3.y = p3.y * p3.z;
 
+	//这段代码不对：虽然X，Y是凸的，但是这里的值实际上是X/Z，Y/Z不是凸的，因此这样找min max不对（当然稠密mesh这样近似是对的）
+	/*
 	int minX = (MIN(p1.x, MIN(p2.x, p3.x)) * fx + cx);
 	int minY = (MIN(p1.y, MIN(p2.y, p3.y)) * fy + cy);
 	int maxX = (MAX(p1.x, MAX(p2.x, p3.x)) * fx + cx) + 0.999999f;
@@ -106,7 +123,31 @@ __global__ void Render_gpu(glm::vec3* positions, glm::ivec3* indices, int* color
 	minY = MAX(0, minY);
 	maxX = MIN(width, maxX);
 	maxY = MIN(height, maxY);
+	*/
 
+	//改进了最小-最大值的求法：如果三个z都是正的就正常来，否则就遍历全局即可
+	int minX = 0;
+	int minY = 0;
+	int maxX = width; 
+	int maxY = height;
+	if(p1.z > 0.0001 && p2.z > 0.0001 && p3.z > 0.0001)
+	{
+		minX = (MIN(p1.x, MIN(p2.x, p3.x)) * fx + cx);
+		minY = (MIN(p1.y, MIN(p2.y, p3.y)) * fy + cy);
+		maxX = (MAX(p1.x, MAX(p2.x, p3.x)) * fx + cx) + 0.999999f;
+		maxY = (MAX(p1.y, MAX(p2.y, p3.y)) * fy + cy) + 0.999999f;
+	
+		minX = MAX(0, minX);
+		minY = MAX(0, minY);
+		maxX = MIN(width, maxX);
+		maxY = MIN(height, maxY);
+	}
+
+
+
+
+
+	
 	for (int py = minY; py <= maxY; ++py) {
 		for (int px = minX; px <= maxX; ++px) {
 			if (px < 0 || px >= width || py < 0 || py >= height)
@@ -116,30 +157,26 @@ __global__ void Render_gpu(glm::vec3* positions, glm::ivec3* indices, int* color
 			float y = (py - cy) / fy;
 
 			glm::dvec3 baryCentricCoordinate = calculateBarycentricCoordinate(p1, p2, p3, glm::dvec3(x, y, 0));
-			/*
-			if (idx == 2766 && px == 128 && py == 128) {
-				printf("Step1\n");
-				printf("========================\n");
-				printf("%f %f %f; %f %f %f; %f %f %f\n", p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
-				printf("%f %f %f %f %f\n", x, y, baryCentricCoordinate.x,baryCentricCoordinate.y,baryCentricCoordinate.z);
-				printf("========================\n");
-			}
-			*/
-			if (isBarycentricCoordInBounds(baryCentricCoordinate)) {
-				int pixel = py * width + px;
 
-				float z = getZAtCoordinate(baryCentricCoordinate, p1, p2, p3);
-				int z_quantize = z * 100000;
+			//这里也不对：X/Z Y/Z不在范围内不等于X,Y,Z实际不在范围内，我选择求出来然后反推
+			//if (isBarycentricCoordInBounds(baryCentricCoordinate)) {
+			int pixel = py * width + px;
+
+			float inv_z = getZAtCoordinate(baryCentricCoordinate, p1, p2, p3);
+			if(inv_z <= 0)
+			{
+				continue;
+			}
+			float real_x = x / inv_z;
+			float real_y = y / inv_z;
+			glm::dvec3 real_coordinate = calculateBarycentricCoordinate(p1_original, p2_original, p3_original, glm::dvec3(real_x, real_y, 0));
+			if(isBarycentricCoordInBounds(real_coordinate)) {
+
+				int z_quantize = inv_z * 100000;
 
 				int original_z = atomicMax(&zbuffer[pixel], z_quantize);
 
 				if (original_z < z_quantize) {
-					/*
-					if (px == 128 && py == 128) {
-						printf("Update %d %d %d %d %d\n", z_quantize, face[0], face[1], face[2], idx);
-						printf("Coordinate %f %f %f\n", baryCentricCoordinate.x, baryCentricCoordinate.y, baryCentricCoordinate.z);
-					}
-					*/
 					glm::vec3 rgb = baryCentricCoordinate;
 					if (render_primitives == 0) {
 						atomicExchRGBZ(&zbuffer[pixel], &color[pixel], z_quantize, CompactRGBToInt(rgb));
@@ -183,7 +220,8 @@ __global__ void FetchVMap_gpu(int* d_z, int* findices, glm::vec3* positions, glm
 	glm::dvec3 p2 = glm::dvec3(rotation * positions[face[1]] + translation);
 	glm::dvec3 p3 = glm::dvec3(rotation * positions[face[2]] + translation);
 
-	if (p1.z < 0.2 || p2.z < 0.2 || p3.z < 0.2) {
+	
+	if (p1.z < 0.0001 && p2.z < 0.0001 && p3.z < 0.0001) {
 		vindices[idx] = glm::ivec3(0, 0, 0);
 		vweights[idx] = glm::vec3(0, 0, 0);
 		return;
@@ -203,19 +241,14 @@ __global__ void FetchVMap_gpu(int* d_z, int* findices, glm::vec3* positions, glm
 
 	glm::dvec3 barycentric = calculateBarycentricCoordinate(p1, p2, p3, glm::dvec3((px - cx) / fx, (py - cy) / fy, 0));
 	double inv_z = 1.0f / getZAtCoordinate(barycentric, p1, p2, p3);
-	/*
-	if (px == 128 && py == 128) {
-		printf("========================\n");
-		printf("%f %f %f; %f %f %f; %f %f %f\n", p1.x, p1.y, p1.z, p2.x, p2.y, p2.z, p3.x, p3.y, p3.z);
-		printf("%f %f %f %f %f\n", (px - cx) / fx, (py - cy) / fy, barycentric.x,barycentric.y,barycentric.z);
-		printf("%f %f %f", barycentric.x * p1.z * inv_z, barycentric.y * p2.z * inv_z, barycentric.z * p3.z * inv_z);
-		printf("%f %f %f %f\n", p1.z, p2.z, p3.z, inv_z);
-		printf("========================\n");
+	
+	if(inv_z < 0)
+	{
+		vindices[idx] = glm::ivec3(0, 0, 0);
+		vweights[idx] = glm::vec3(0, 0, 0);
+		return;
 	}
-	if (!(inv_z < 1e6)) {
-		printf("Warning!\n");
-	}
-	*/
+
 	vweights[idx] = glm::vec3(barycentric.x * p1.z * inv_z, barycentric.y * p2.z * inv_z, barycentric.z * p3.z * inv_z);
 }
 
